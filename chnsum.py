a207510f3930be081a0f1074b523fb4de39e2a06
Haoran Liao <haoran@physics.mcgill.ca>
2018-01-17 14:36:34 -0500

    updates

--- chnsum.py
+++ chnsum.py
@@ -2,17 +2,20 @@ import glob
 import Chn
 import matplotlib.pyplot as matplot
 
-chnfiles = glob.glob("*.Chn")
-chnfiles.sort()
-
-for i in range(len(chnfiles)):
-    current = Chn.Chn(chnfiles[i])
-    if 0 == i:
-        spectrum = current.spectrum
-    else:
-        spectrum = spectrum + current.spectrum
-
-matplot.plot(spectrum)
-matplot.xlabel("Channel")
-matplot.ylabel("Count")
-matplot.show()
+def chnsum():
+    chnfiles = glob.glob("*.Chn")
+    chnfiles.sort()
+    
+    for i in range(len(chnfiles)):
+        current = Chn.Chn(chnfiles[i])
+        if 0 == i:
+            spectrum = current.spectrum
+        else:
+            spectrum = spectrum + current.spectrum
+    
+    matplot.plot(spectrum)
+    matplot.xlabel("Channel")
+    matplot.ylabel("Count")
+    matplot.show()
+    
+    return spectrum
