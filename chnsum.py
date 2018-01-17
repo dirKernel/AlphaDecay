6408544cf619efd50844f8f2ba498a53ef9f404f
Haoran Liao <haoran@physics.mcgill.ca>
2018-01-17 14:57:56 -0500

    added halflifeMeasurement()

--- chnsum.py
+++ chnsum.py
@@ -1,21 +1,23 @@
-import glob
+#import glob
+import os
 import Chn
 import matplotlib.pyplot as matplot
 
-def chnsum():
-    chnfiles = glob.glob("*.Chn")
+def chnsum(folderName):
+    chnfiles = os.listdir(folderName)
+#    chnfiles = glob.glob("*.Chn")
     chnfiles.sort()
     
     for i in range(len(chnfiles)):
-        current = Chn.Chn(chnfiles[i])
+        current = Chn.Chn(folderName+'/'+chnfiles[i])
         if 0 == i:
             spectrum = current.spectrum
         else:
             spectrum = spectrum + current.spectrum
     
-    matplot.plot(spectrum)
-    matplot.xlabel("Channel")
-    matplot.ylabel("Count")
-    matplot.show()
+#    matplot.plot(spectrum)
+#    matplot.xlabel("Channel")
+#    matplot.ylabel("Count")
+#    matplot.show()
     
     return spectrum
