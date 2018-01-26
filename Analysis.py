d89d17c8533ff86e6b1bd624e726ee0acea9aa6d
Haoran Liao <haoran@physics.mcgill.ca>
2018-01-26 15:03:53 -0500

    updates

--- Analysis.py
+++ Analysis.py
@@ -3,8 +3,8 @@ import Chn
 import pylab as plb
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-mpl.rcParams['axes.linewidth'] = 1.3 #set the value globally
-mpl.rcParams.update({'font.size': 12})
+mpl.rcParams['axes.linewidth'] = 1.5 #set the value globally
+mpl.rcParams.update({'font.size': 13})
 from scipy.optimize import curve_fit
 import scipy as sp
 import math
@@ -76,19 +76,23 @@ def linearFit(x, y, yerr):
 def gauss(x, a, mean, sigma):
     return a*np.exp(-(x-mean)**2/(2*sigma**2))
 
-def gaussTwo(x, *params):
-    y = np.zeros_like(x)
-    for i in range(0, len(params), 4):
-        a = params[i]
-        mean = params[i+1]
-        sigma = params[i+2]
-        y = y + a*np.exp(-(x-mean)**2/(2*sigma**2))
-    return y
+def gaussMul(x, *params, sigmaFixed=False):
+    if not sigmaFixed:
+        y = np.zeros_like(x)
+        for i in range(0, len(params), 3):
+            a = params[i]
+            mean = params[i+1]
+            sigma = params[i+2]
+            y = y + a*np.exp(-(x-mean)**2/(2*sigma**2))
+        return y
+    else:
+        a1, mean1, sigma, a2, mean2 = params[0], params[1], params[2], params[4], params[5]  
+        return a1*np.exp(-(x-mean1)**2/(2*sigma**2))+a2*np.exp(-(x-mean2)**2/(2*sigma**2))
 
 def expGauss(x, A, l, s, m):
     return A*l/2*np.exp(l/2*(2*x-2*m+l*s*s))*(1-sp.special.erf((x+l*s*s-m)/(math.sqrt(2)*s)))    
 
-def expGaussTwo(x, *params):
+def expGaussMul(x, *params):
     y = np.zeros_like(x)
     for i in range(0, len(params), 4):
         A = params[i]
@@ -98,14 +102,14 @@ def expGaussTwo(x, *params):
         y = y + A*l/2*np.exp(l/2*(2*x-2*m+l*s*s))*(1-sp.special.erf((x+l*s*s-m)/(math.sqrt(2)*s)))
     return y
 
-def expGaussFitTwo(filePathtobeSaved, x, y, yerr, p0, x0, left, res_tick=[-3,0,3]):
+def expGaussFitMul(filePathtobeSaved, x, y, yerr, p0, x0, left, res_tick=[-3,0,3]):
     fig = plt.figure(figsize=(8, 6))
-    popt, pcov = curve_fit(expGaussTwo, x, y, p0=p0, maxfev=50000)
-    npara = 8
-    rchi, dof = reducedChiSquare(y, expGaussTwo(x, *popt), yerr, npara)
+    popt, pcov = curve_fit(expGaussMul, x, y, p0=p0, maxfev=50000)
+    npara = 12
+    rchi, dof = reducedChiSquare(y, expGaussMul(x, *popt), yerr, npara)
     plt.errorbar(x+x0-left, y, yerr=yerr,fmt='x', elinewidth=1 ,capsize=2, ecolor='b', \
                  label='Data', linestyle='None', markersize=5,color='k')
-    plt.plot(x+x0-left, expGaussTwo(x, *popt), '-r', label='Fit')
+    plt.plot(x+x0-left, expGaussMul(x, *popt), '-r', label='Fit')
     plt.legend()
     plt.xlabel('Channels')
     plt.ylabel('Counts')
@@ -118,11 +122,15 @@ def expGaussFitTwo(filePathtobeSaved, x, y, yerr, p0, x0, left, res_tick=[-3,0,3
     print('Sigma 2: %f $\pm$ %f'%(popt[6], perr[6]))
     print('Lambda 2: %f $\pm$ %f'%(popt[5], perr[5]))
     print('A 2: %f $\pm$ %f'%(popt[4], perr[4]))
+    print('Mean 3 (Scaled): %f $\pm$ %f'%(popt[11], perr[11]))
+    print('Sigma 3: %f $\pm$ %f'%(popt[10], perr[10]))
+    print('Lambda 3: %f $\pm$ %f'%(popt[9], perr[9]))
+    print('A 3: %f $\pm$ %f'%(popt[8], perr[8]))
     print('RChi: %f'%(rchi))
     print('DOF: %d'%(dof))
     
     # Plot residuals
-    d = y-expGaussTwo(x,*popt)
+    d = y-expGaussMul(x,*popt)
     stu_d = d/np.std(d, ddof=1) # studentized residual
     stu_d_err = yerr/np.std(d, ddof=1)
     axes = plt.gca()
@@ -159,7 +167,7 @@ def gaussianFit(filePathtobeSaved, x, y, yerr, p0=[300, 20, 2.5], left=15, right
     npara = 3
     rchi, dof = reducedChiSquare(yy, gauss(xx, *popt), yerr, npara)
     perr = np.sqrt(np.diag(pcov))
-    plt.errorbar(xx+x0-left, yy, yerr=yerr,fmt='x', elinewidth=1 ,capsize=2, ecolor='b', \
+    plt.errorbar(xx+x0-left, yy, yerr=yerr,fmt='x', elinewidth=1.5 ,capsize=2, ecolor='b', \
                  label='Data', linestyle='None', markersize=5, color='k')
     xxx = np.linspace(min(xx),max(xx),1000)
     plt.plot(xxx+x0-left, gauss(xxx, *popt), 'r-', label='Fit')
@@ -183,7 +191,7 @@ def gaussianFit(filePathtobeSaved, x, y, yerr, p0=[300, 20, 2.5], left=15, right
     axes2.axhline(y=0, color='r', linestyle='-')
     axes.tick_params(axis='both', direction='in')
     axes2.tick_params(axis='both', direction='in')
-    axes2.errorbar(xx+x0-left, stu_d, yerr=stu_d_err, fmt='x', elinewidth=1 ,capsize=2, ecolor='b', \
+    axes2.errorbar(xx+x0-left, stu_d, yerr=stu_d_err, fmt='x', elinewidth=1.5 ,capsize=2, ecolor='b', \
                  label='Data', linestyle='None', markersize=5,color='k')
     
     func = 'A*exp(-(x-mean)^2/(2*sigma^2))'
@@ -205,6 +213,64 @@ def gaussianFit(filePathtobeSaved, x, y, yerr, p0=[300, 20, 2.5], left=15, right
 
     return popt, perr, rchi
 
+def gaussianFitMul(filePathtobeSaved, x, y, yerr, p0, left=15, right=15, res_tick=[-3,0,3]):
+    fig = plt.figure(figsize=[8, 6])
+    ind = np.argmax(y) #to get the peak value x-coord
+    x0 = x[ind] #x0 is the peak value x-coord (channel number)
+    yy = y[x0-left:x0+right]
+    xx = np.arange(len(yy))
+    yerr = yerr[x0-left:x0+right]
+    popt, pcov = curve_fit(gaussMul, xx, yy, p0=p0, maxfev=500000) #initial guess of the amplitude is 100, mean is x0 and variance (sigma) 5
+    print(popt)
+    npara = 6
+    rchi, dof = reducedChiSquare(yy, gaussMul(xx, *popt), yerr, npara)
+    perr = np.sqrt(np.diag(pcov))
+    plt.errorbar(xx+x0-left, yy, yerr=yerr,fmt='x', elinewidth=2 ,capsize=3, ecolor='b', \
+                 label='Data', linestyle='None', markersize=5, color='k')
+    xxx = np.linspace(min(xx),max(xx),1000)
+    plt.plot(xxx+x0-left, gaussMul(xxx, *popt), 'r-', label='Fit', linewidth=1.5)
+    plt.legend()
+    plt.xlabel('Channels')
+    plt.ylabel('Counts')
+    
+    # Plot residuals
+    d = yy-gaussMul(xx,*popt)
+    stu_d = d/np.std(d, ddof=1)
+    stu_d_err = yerr/np.std(d, ddof=1)
+    axes = plt.gca()
+    divider = make_axes_locatable(axes)
+    axes2 = divider.append_axes("top", size="20%", pad=0.1)
+    axes.figure.add_axes(axes2)
+    axes2.set_xlim(axes.get_xlim())
+    axes2.set_yticks(res_tick)
+    axes.tick_params(width=1.3, axis='both', direction='in', bottom=True, top=True, left=True, right=True)
+    axes2.tick_params(width=1.3, axis='both', direction='in', bottom=True, top=True, left=True, right=True, labelbottom=False)
+    axes2.set_ylabel('Studentized\nResidual', color='k')
+    axes2.axhline(y=0, color='r', linestyle='-')
+    axes.tick_params(axis='both', direction='in')
+    axes2.tick_params(axis='both', direction='in')
+    axes2.errorbar(xx+x0-left, stu_d, yerr=stu_d_err, fmt='x', elinewidth=2 ,capsize=3, ecolor='b', \
+                 label='Data', linestyle='None', markersize=5,color='k')
+    
+    textstr = '$\chi^2$=%.2f\tDOF=%d'%(rchi, dof)
+    plt.text(0.1, 0.9, textstr, fontsize=13, transform=plt.gcf().transFigure)
+    
+    print('A 1: %f $\pm$ %f'%(popt[0], perr[0]))
+    print('Mean 1: %f\pm%f'%(popt[1], perr[1]))
+    print('Sigma 1: %f $\pm$ %f'%(popt[2], perr[2]))
+    print('A 2: %f $\pm$ %f'%(popt[3], perr[3]))
+    print('Mean 2: %f\pm%f'%(popt[4], perr[4]))
+    print('Sigma 2: %f $\pm$ %f'%(popt[5], perr[5]))
+    print('A 3: %f $\pm$ %f'%(popt[6], perr[6]))
+    print('Mean 3: %f\pm%f'%(popt[7], perr[7]))
+    print('Sigma 3: %f $\pm$ %f'%(popt[8], perr[8]))
+    print('RChi: %f'%(rchi))
+    print('DOF: %d'%(dof))
+    plt.show()
+    fig.savefig(filePathtobeSaved+'.eps', format='eps', dpi=1000, bbox_inches='tight', pad_inches=0.0)
+
+    return popt, perr, rchi, dof
+
 def fitAlphaPeaks(filePathtobeSaved, filePath, p0, left=100, right=100, res_tick=[-3,0,3]):
     """
     This is the function to fit Alpha Peak
@@ -221,11 +287,32 @@ def fitAlphaPeaks(filePathtobeSaved, filePath, p0, left=100, right=100, res_tick
     yy = y[x0-left:x0+right]
     xx = np.arange(len(yy))
     yerr = np.sqrt(yy)
-    popt, perr, rchi, dof = expGaussFitTwo(filePathtobeSaved, xx, yy, yerr, p0, x0, left, res_tick)
+    popt, perr, rchi, dof = expGaussFitMul(filePathtobeSaved, xx, yy, yerr, p0, x0, left, res_tick)
     popt[3] += x0-left
     popt[7] += x0-left
     print('Mean 1 (Not Scaled): %f \pm %f'%(popt[3], perr[3]))
     print('Mean 2 (Not Scaled): %f \pm %f'%(popt[7], perr[7])+'\n')
+    
+    return popt, perr, rchi, dof
+
+def fitAlphaPeaksGaussMul(filePathtobeSaved, filePath, p0, left=100, right=100, res_tick=[-3,0,3]):
+    """
+    This is the function to fit Alpha Peak
+    filepath: full path to the file; p0: list of initial guess; left: how much away
+    to the left from peak channel; right: how much away to the right from peak channel
+    res_tick: the residual plot y axis ticks
+    """
+    ch = Chn.Chn(filePath)
+    y = ch.spectrum
+    x = np.arange(len(y))
+    ind = np.argmax(y)
+    x0 = x[ind]
+    yerr = np.sqrt(y)
+    popt, perr, rchi, dof = gaussianFitMul(filePathtobeSaved, x, y, yerr, p0, left, right, res_tick)
+    popt[1] = popt[1]+x0-left
+    popt[4] = popt[4]+x0-left
+    print('Mean 1 (Not Scaled): %f \pm %f'%(popt[1], perr[1]))
+    print('Mean 2 (Not Scaled): %f \pm %f'%(popt[4], perr[4])+'\n')
     
     return popt, perr, rchi, dof
 
@@ -477,8 +564,11 @@ def halflifeMeasurement(outFileName, folderName):
 #m_calib, m_calib_e, b_calib, b_calib_e = calibratePulses('CalibrationWBias_2')
 #m_press, m_press_e, b_press, b_press_e = pressureData('Pressure_2')
 
-popt_am, perr_am, rchi_am, dof_am= fitAlphaPeaks("Figures/Calibration/Americium_300_sec.Chn", "Americium/Americium_300_sec.Chn", \
-                         [785, 1, 1, 65, 1750, 1, 1.6, 70], left=70, right=30, res_tick=[-2,0,2])
+#popt_am, perr_am, rchi_am, dof_am= fitAlphaPeaks("Figures/Calibration/Americium_300_sec.Chn", "Americium/Americium_300_sec.Chn", \
+#                         [100, 0.1, 2, 50, 785, 2, 0.8, 60, 1750, 2, 1.6, 70], left=70, right=30, res_tick=[-2,0,2])
+popt_am, perr_am, rchi_am, dof_am= fitAlphaPeaksGaussMul("Figures/Calibration/Americium_300_sec.Chn", "Americium/Americium_300_sec.Chn", \
+                         [8, 50, 3, 60, 60, 3, 310, 70, 2], left=70, right=30, res_tick=[-5,0,5])
+
 #m_am, m_am_e = popt_am[3], perr_am[3]
 #print('Amerisium Calibration: Mean channel = %f $\pm$ %f\nFit function = %s'%\
 #      (m_am, m_am_e, func_am))
